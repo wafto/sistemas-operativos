d41243e883bde0305760c67d18f65848597c9fb9
Aftab Hussain Miranda <wafto.mx@gmail.com>
2012-05-24 20:57:48 -0500

    starting some tests for paginacion

---
+++ paginacion/control.c
@@ -0,0 +1,17 @@
+/* control.c
+ * 
+ * 
+ * Autor: Aftab Hussain Miranda
+ * Fecha de Elaboracion: 24 de Mayo del 2012.
+ *  
+ * Repositorio: https://github.com/wafto/sistemas-operativos
+ * Compilacion: 
+ * Ejecucion:
+ */
+ #include "lib/particiones.h"
+
+ int main() {
+
+
+ 	return 0;
+ }
