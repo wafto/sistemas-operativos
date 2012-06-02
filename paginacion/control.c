147875ca6653243a989365918bab6a00907a0b5d
Aftab Hussain Miranda <wafto.mx@gmail.com>
2012-06-02 13:21:58 -0500

    deleting useless code

--- paginacion/control.c
+++ paginacion/control.c
@@ -12,53 +12,5 @@
 
 int main() {
 
-	Paginacion paginacion;
-	int err, i;
-	struct timespec tim, tim2;
-	int tiempo = 1;
-   	tim.tv_sec = 0;
-   	tim.tv_nsec = tiempo * 100000000;
-
-	inicializarPaginacion(&paginacion, 1000, 20);
-	
-	printf("\n");
-	agregarSolicitud(&paginacion, 60, "aftab");
-	agregarSolicitud(&paginacion, 130, "hussain");
-	agregarSolicitud(&paginacion, 120, "saass");
-	agregarSolicitud(&paginacion, 270, "foo");
-	agregarSolicitud(&paginacion, 140, "bar");
-	agregarSolicitud(&paginacion, 240, "tazuu");
-	agregarSolicitud(&paginacion, 120, "batfa");
-	agregarSolicitud(&paginacion, 130, "miranda");
-	agregarSolicitud(&paginacion, 150, "flemx");
-	agregarSolicitud(&paginacion, 170, "lorem");
-	agregarSolicitud(&paginacion, 190, "ipsum");
-	agregarSolicitud(&paginacion, 220, "lol");
-	imprimeTablaSolicitudes(paginacion);
-
-	printf("\n");
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	cargarSolicitud(&paginacion, &err);
-	imprimeTablaProcesos(paginacion);
-
-	for (i = 0; i < 450; i++) {
-		system("clear");
-		imprimeTablaMemorias(paginacion);
-		quantum(&paginacion, &err);
-		printf("%d\n", i);
-		if (err == NO_PROCESOS) break;
-		nanosleep(&tim , &tim2);
-	}
-	
 	return 0;
 }
